5981b820f145cbc7c468c1d0345f73255ba18a97
Wenhao Ge <ge.wen@northeastern.edu>
2021-10-10 15:33:13 -0700

    working on CRUD operations

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.product_index, name="product_index"),
     path("<int:pk>/", views.product_detail, name="product_detail"),
+    path("create/", views.create_product, name="create_product")
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, HttpResponseRedirect)
 from products.models import Product
+from .forms import ProductForm
 
 
 def product_index(request):
@@ -18,3 +19,35 @@ def product_detail(request, pk):
     return render(request, 'product_detail.html', context)
 
 
+def edit_product(request, pk):
+
+    # product = get_object_or_404(Product, pk = pk)
+    #
+    # new_product = Product(request.POST or None, instance = product)
+    #
+    # if new_product.is_valid():
+    #     new_product.save()
+    #     return HttpResponseRedirect("/" + pk)
+    #
+    # context = {
+    #     'product': product
+    # }
+
+    return render(request, 'product_index.html', {})
+
+
+def create_product(request):
+
+    if request.method == "POST":
+        product_form = ProductForm(data=request.POST)
+        if product_form.is_valid():
+            product_form = product_form.save()
+            return HttpResponseRedirect("/")
+    else:
+        product_form = ProductForm()
+
+    return render(request, "create_product.html", {"product": product_form})
+
+
+def delete_product(request, id):
+    pass
